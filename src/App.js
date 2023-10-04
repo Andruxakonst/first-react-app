adf5d3d15d68cc9e8cc1a99774b1dbb619506c5f
andruxakonst <andruxakonst@yandex.ru>
2023-10-04 20:08:43 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import CostItem from "./components/CostItem";
 function App() {
+    const costs = [
+        {
+            date : new Date(2021, 2, 12),
+            description : 'Холодильник',
+            price : 999.99,
+        },
+        {
+            date : new Date(2021, 11, 25),
+            description : 'MacBook',
+            price : 1254.72,
+        },
+        {
+            date : new Date(2021, 3, 1),
+            description : 'Джинсы',
+            price : 49.99,
+        },
+    ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Начнем изучение REACT</h1>
+        <CostItem
+            date={costs[0].date}
+            description={costs[0].description}
+            price={costs[0].price}>
+        </CostItem>
+        <CostItem
+            date={costs[1].date}
+            description={costs[1].description}
+            price={costs[1].price}>
+        </CostItem>
+        <CostItem
+            date={costs[2].date}
+            description={costs[2].description}
+            price={costs[2].price}>
+        </CostItem>
     </div>
   );
 }
