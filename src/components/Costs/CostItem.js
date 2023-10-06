0670d7ca337986b9815972e443bfc9edb2a665bb
andruxakonst <andruxakonst@yandex.ru>
2023-10-06 12:38:51 +0300

    Обработка событий

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -2,7 +2,9 @@ import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
 const CostItem = (props) => {
-
+    const chengeDescriptionHandler = ()=>{
+        console.log("Click");
+    }
     return(
         <Card className={'cost-item'}>
             <CostDate date={props.date}></CostDate>
@@ -10,6 +12,7 @@ const CostItem = (props) => {
                 <h2>{props.description}</h2>
                 <div className={'cost-item__price'}>${props.price}</div>
             </div>
+            <button onClick={chengeDescriptionHandler}>Изменить описание</button>
         </Card>
     );
 }
