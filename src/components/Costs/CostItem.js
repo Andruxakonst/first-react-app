b584891c2b5a9f26de3831ded8d75437814f289a
andruxakonst <andruxakonst@yandex.ru>
2023-10-06 14:17:02 +0300

    Обработка событий и работа с состоянием

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,15 +1,23 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
+import {useState} from "react"; //Импортируем из Ract useSatate
 const CostItem = (props) => {
+
+    //const [Пполучаемая переменная, функция для установки нового значения] = useState(Переменная за которой следим для обновления интерфейса компонента)
+    const [description, setDescription] = useState(props.description)
+
+    //обрабатываем Click на элементе button
     const chengeDescriptionHandler = ()=>{
-        console.log("Click");
+        //обновляем значение переменной props.description через setDescription
+        //что вызовет обновление интерфейса комонента с заменой переменной
+        setDescription(description+" New cost");
     }
     return(
         <Card className={'cost-item'}>
             <CostDate date={props.date}></CostDate>
             <div className={'cost-item__description'}>
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
                 <div className={'cost-item__price'}>${props.price}</div>
             </div>
             <button onClick={chengeDescriptionHandler}>Изменить описание</button>
